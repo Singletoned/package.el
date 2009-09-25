a10184ab3a39ab84be0427762e317a272beefbc2
Ed Singleton <singletoned@gmail.com>
2009-09-25 17:17:23 +0100

    Added some packages

---
+++ elpa/yasnippet-bundle-0.5.10/yasnippet-bundle-autoloads.el
@@ -0,0 +1,25 @@
+;;; yasnippet-bundle-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil "yasnippet-bundle" "yasnippet-bundle.el" (18981
+;;;;;;  23765))
+;;; Generated autoloads from yasnippet-bundle.el
+(require 'yasnippet-bundle)
+
+;;;***
+
+;;;### (autoloads nil nil ("yasnippet-bundle-pkg.el") (18981 23766
+;;;;;;  166231))
+
+;;;***
+
+(provide 'yasnippet-bundle-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; yasnippet-bundle-autoloads.el ends here
